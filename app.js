d97ed9d1c46f468065f149647ef2540f7d8e6dd2
Ijas mohammed <ijas.mi2714@gmail.com>
2021-01-25 14:11:32 +0530

    feat:ex1-ex3

---
+++ app.js
@@ -0,0 +1,11 @@
+// var userName= prompt("what is your name");
+// alert("this script works " +userName)
+var btnTrans=document.querySelector("#btn");
+
+
+btnTrans.addEventListener("click",clickHandler)
+
+function clickHandler()
+{
+    console.log("clicked");
+}
